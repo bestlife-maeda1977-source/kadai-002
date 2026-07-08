d612b791a74fec7b217f34e8be428b6dd4ab1efb
bestlife.maeda1977 <bestlife.maeda1977.gmail.com>
2026-07-08 10:13:07 +0900

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        // SCSSをコンパイルして生成されるCSSファイル名を固定する設定
+        assetFileNames: "assets/[name][extname]",
+      },
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: [
+          "color-functions",
+          "import",
+          "global-builtin",
+          "slash-div",
+        ],
+      },
+    },
+  },
+});
