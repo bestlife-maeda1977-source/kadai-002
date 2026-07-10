2e2ec6019bd57eb5054729679c9784c6dfd942a4
bestlife.maeda1977 <bestlife.maeda1977.gmail.com>
2026-07-10 12:18:41 +0900

    いったんコミット

--- src/main.js
+++ src/main.js
@@ -1,2 +1,14 @@
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./style.scss";
+
+$(document).ready(function() {
+  var $grid = $('.grid').imagesLoaded(function() {
+    $grid.masonry({
+      itemSelector: '.grid-item',
+      percentPosition: true,
+      horizontalOrder: true,
+      fitWidth: true,
+      // columnWidthはCSSで制御するため指定しなくてOK
+    });
+  });
+});
