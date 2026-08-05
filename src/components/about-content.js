8188ac95368b6cd6352b2786c981214cbad86d22
bestlife-maeda1977-source <bestlife.maeda1977@gmail.com>
2026-08-05 11:51:14 +0900

    ABOUT：マークアップ完了

--- src/components/about-content.js
+++ src/components/about-content.js
@@ -1,6 +1,6 @@
 class AboutContent extends HTMLElement {
-  connectedCallback() {
-    this.innerHTML = `
+	connectedCallback() {
+		this.innerHTML = `
       <main class="main-about">
         <section class="container-xl container-about">
           <div class="row">
@@ -8,7 +8,112 @@ class AboutContent extends HTMLElement {
           </div>
         </section>
       </main>
+      <main class="main-about">
+        <div class="container-about">
+          <div class="container-xl header">
+            <div class="row">
+              <section class="col intro">
+                <h2 class="font-EBGaramond">INTRO</h2>
+                <h3>”伝統で無いものは、すべて盗作である” – ユージェニオ・ドール</h3>
+                <p>ピエール・フルニエが1994年にフランス・パリでANATOMICAをオープンしたのは、当時メンズシーンから失われていた、または失われつつあった伝統的なヨーロッパのファッションを蘇らせるためだった。歴史と伝統、そして自身によるフィット感に拘った「アナトミカル（解剖学的）」なコレクションを展開。その後、2006年の寺本欣児との出会いによってANATOMICAに新たな方向性が加わる。2人のフィルターを通した、日本のものづくりによるアメリカンテイスト・ガーメントの誕生となる。</p>
+              </section>
+              <section class="col concept">
+                <h2 class="font-EBGaramond">CONCEPT</h2>
+                <p>ヨーロッパ、アメリカ及びアジアの歴史と伝統、そしてフィッティングへの拘りをベースに、最上の素材を使用し、人体の構造と動きに沿ったアナトミカル（解剖学的）なモノづくりをコンセプトとする。時代や物事に流されない普遍的で且つエレガントなオリジナルコレクションを展開する。ANATOMICAを象徴するロゴマークには、木靴を照らす王冠のように「人々を照らす永遠のワードローブでありたい」というピエールの想いが込められている。</p>
+              </section>
+            </div>
+          </div>
+
+          <div class="container-xl history">
+            <div class="row">
+              <section class="col">
+                <h2 class="font-EBGaramond">BRAND HISTORY</h2>
+                <div class="history-grid">
+                  <div class="history-block">
+                    <div class="history-item">
+                      <dl class="history-item-header">
+                        <dt>1975</dt>
+                        <dd>ANATOMICA、フランスはパリで産声をあげる</dd>
+                      </dl>
+                      <p>ピエールは海外生産や工程の簡略化によって品質が急激に低下した外部委託生産を取り止め、自身のコレクションを本国フランスで生産することを決意。HEMISPHERE閉店の翌年、HEMISPHEREからの付き合いであるジルベール・ヌースと共にANATOMICAを立ち上げ、パリ・4区に店を開店する。そのコレクションは、自身のアーカイヴをオマージュしながらフランスの伝統を守り抜き、そして更なるフィット感への拘りをベースとした、人体の構造と動きに沿った作品作りであり、伝統的な作業着や軍服のディテールを忠実に再現することを理念とする。</p>
+                    </div>
+                    <div class="history-item">
+                      <dl class="history-item-header">
+                        <dt>1979</dt>
+                        <dd>パリ・16区にHEMISPHEREをオープン</dd>
+                      </dl>
+                      <p>パリ・16区のグランド・アルメ通りに、北半球を意味するHEMISPHERE（エミスフェール）を開店。当時サンローランのナンバー3として活躍していたジャン・セバスチャンがピエール・フルニエのセンスに目を付け、HEMISPHEREオープンに至る。アメリカでも限られた人の目にしか触れられていなかった民芸ブランド等を世界に広めたのも同店であり、日本進出も果たす。1993年、共同経営者の逝去により閉店。</p>
+                    </div>
+                  </div>
+                  <div class="history-block">
+                    <div class="history-item">
+                      <dl class="history-item-header">
+                        <dt>1994</dt>
+                        <dd>ANATOMICA、フランスはパリで産声をあげる</dd>
+                      </dl>
+                      <p></p>
+                      <img class="image-fluid" src="/src/assets/about/img-history-2.webp" alt="ANATOMICA PARIS" loading="lazy" decoding="async" />
+                      <dl class="history-item-header">
+                        <dt>2006</dt>
+                        <dd>ピエールと寺本が出会う</dd>
+                      </dl>
+                      <p>ANATOMICAを訪れた寺本欣児のジーンズが、ピエールの目に留まる。アパレル・デザイナーである寺本は孤高のヴィンテージコレクターでもあり、この時サイドシームが全くないジーンズを着用していた。それまでフランスのヴィンテージに注力し、アメリカンヴィンテージには背を向けてきたピエールだったが、彼が忘れようとしていた記憶と情熱が再燃。そして、ANATOMICAブランド初のジーンズを開発することになる。ピエールと寺本が共同でデザインするアメリカンテイスト・ガーメント誕生の瞬間である。<br />このANATOMICAブランド初のジーンズ「618 ORIGINAL」は、15ヶ月以上もの月日をかけて2007年秋に完成。その後ピエールと寺本がパートナー契約を交わし、2人のフィルターを通した、日本のモノづくりによるアメリカンテイスト・ガーメントがANATOMICAの新たな方向性に加わる。2人が好む1960年代のアメリカンテイストから、ミリタリーアイテム、イギリスのトラディショナルなアイテム、スニーカー、レディースラインの代表作マリリンと、次々にヒット作を生み出す。</p>
+                    </div>
+                  </div>
+                  <div class="history-block">
+                    <div class="history-item">
+                      <dl class="history-item-header">
+                        <dt>2011</dt>
+                        <dd>東京・東日本橋にANATOMICA TOKYOをオープン</dd>
+                      </dl>
+                      <p>2008年、華やかな神宮前エリアの商業ビルにショップインショップとして日本1号店を開店。この経験が、独立店舗を立ち上げたいというピエールと寺本の想いに火をつけ、程なく、江戸時代から商業の中心地であった東日本橋に、旗艦店に相応しい物件を見つける。2011年、ANATOMICA TOKYOを開店し、札幌、名古屋、神戸、青山、福岡に次々とショップをオープンする。</p>
+                      <img class="image-fluid" src="/src/assets/about/img-history-2.webp" alt="ANATOMICA TOKYO" loading="lazy" decoding="async" />
+                    </div>
+                  </div>
+                </div>
+              </section>
+            </div>
+          </div>
+
+          <section class="container-xl key-person">
+            <div class="row">
+              <div class="col">
+                <div class="flex">
+                  <div class="flex-item">
+                    <div class="img">
+                      <img class="image-fluid" src="/src/assets/about/img-keyperson-1.webp" alt="PIERRE FOURNIER(ピエール・フルニエ)" loading="lazy" decoding="async" />
+                    </div>
+                    <div class="detail">
+                      <h3>
+                        <span class="jp">ピエール・フルニエ</span>
+                        <span class="en">PIERRE FOURNIER</span>
+                      </h3>
+                      <p>1944年フランス生まれ。1975年パリ・レアール地区に「GLOBE」、1979年パリ・16区に「HEMISPHERE」、1994年にANATOMICAを立ち上げ、世界のアパレル業界に最も大きな影響を与えた人物の一人。その場所やその民族でしか生まれてこないオリジナルに興味を持ち、数々の「本物」をアパレル業界に送り出してきた。ANATOMICAのディレクターとしてブランドのモノづくりを指揮し、デザイナー寺本欣児と共にANATOMICAL UTOPIAを目指す。</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col">
+                <div class="flex">
+                  <div class="flex-item">
+                    <div class="img">
+                      <img class="image-fluid" src="/src/assets/about/img-keyperson-2.webp" alt="寺本 欣児（KINJI TERAMOTO）" loading="lazy" decoding="async" />
+                    </div>
+                    <div class="detail">
+                      <h3>
+                      <span class="jp">寺本 欣児</span>
+                      <span class="en">KINJI TERAMOTO</span>
+                      </h3>
+                      <p>1964年兵庫県生まれ。60歳の夏で35周年を迎えるという独自のカウント方式で、1989年、当時25歳で「35SUMMERS」を設立。Rocky Mountain Featherbed、BIG YANKを始めとする所有ブランドを日本国内や海外で展開。アパレル・デザイナーでもあり、ヴィンテージコレクターとしても世界中に名を馳せる。自身が若い時に出会い、多大な影響を受けたピエール・フルニエとパートナーシップを組み、ANATOMICAのデザインや生産、そして日本展開を担う。</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      </main>
     `;
-  }
+	}
 }
-customElements.define('about-content', AboutContent);
+customElements.define("about-content", AboutContent);
