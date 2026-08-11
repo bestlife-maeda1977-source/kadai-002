c490020bb55a5caf8ec63f7e4b1bbafe7ca27c3c
bestlife-maeda1977-source <bestlife.maeda1977@gmail.com>
2026-08-11 12:41:53 +0900

    stockist:マークアップ途中

--- src/components/stockist-content.js
+++ src/components/stockist-content.js
@@ -8,6 +8,236 @@ class StockistContent extends HTMLElement {
               <h1 class="font-EBGaramond">FLAGSHIP STORE</h1>
             </div>
           </div>
+          <div class="row">
+            <div class="col">
+              <div class="d-flex">
+                <div class=flex-item">
+                  <img src="src/assets/stockist/img-stockist-1.webp" alt="Store" class="image-fluid" loading="lazy" decoding="async" />
+                  <h2 class="font-EBGaramond"></h2>
+                  <ul>
+                  <li></li>
+                  <li>TEL : </li>
+                  <li>OPEN : </li>
+                  </ul>
+                  <footer class="footer-stockist">
+                  <a href="" target="_blank" rel="noopener noreferrer">Google Maps</a>
+                  <a href="" width="29" height="29" target="_blank" rel="noopener noreferrer"><img src="src/assets/logo-instagram.svg" alt="Instagram" /></a>"
+                  </footer>
+                </div>
+
+                <div class=flex-item">
+                  <img src="src/assets/stockist/img-stockist-2.webp" alt="Store" class="image-fluid" loading="lazy" decoding="async" />
+                  <h2 class="font-EBGaramond"></h2>
+                  <ul>
+                  <li></li>
+                  <li>TEL : </li>
+                  <li>OPEN : </li>"
+                  </ul>
+                  <footer class="footer-stockist">
+                  <a href="" target="_blank" rel="noopener noreferrer">Google Maps</a>
+                  <a href="" width="29" height="29" target="_blank" rel="noopener noreferrer"><img src="src/assets/logo-instagram.svg" alt="Instagram" /></a>"
+                  </footer>
+                </div>
+
+                <div class=flex-item">
+                  <img src="src/assets/stockist/img-stockist-3.webp" alt="Store" class="image-fluid" loading="lazy" decoding="async" />
+                  <h2 class="font-EBGaramond"></h2>
+                  <ul>
+                  <li></li>
+                  <li>TEL : </li>
+                  <li>OPEN : </li>"
+                  </ul>
+                  <footer class="footer-stockist">
+                  <a href="" target="_blank" rel="noopener noreferrer">Google Maps</a>
+                  <a href="" width="29" height="29" target="_blank" rel="noopener noreferrer"><img src="src/assets/logo-instagram.svg" alt="Instagram" /></a>"
+                  </footer>
+                </div>
+
+                <div class=flex-item">
+                  <img src="src/assets/stockist/img-stockist-4.webp" alt="Store" class="image-fluid" loading="lazy" decoding="async" />
+                  <h2 class="font-EBGaramond"></h2>
+                  <ul>
+                  <li></li>
+                  <li>TEL : </li>
+                  <li>OPEN : </li>"
+                  </ul>
+                  <footer class="footer-stockist">
+                  <a href="" target="_blank" rel="noopener noreferrer">Google Maps</a>
+                  <a href="" width="29" height="29" target="_blank" rel="noopener noreferrer"><img src="src/assets/logo-instagram.svg" alt="Instagram" /></a>"
+                  </footer>
+                </div>
+
+                <div class=flex-item">
+                  <img src="src/assets/stockist/img-stockist-4.webp" alt="Store" class="image-fluid" loading="lazy" decoding="async" />
+                  <h2 class="font-EBGaramond"></h2>
+                  <ul>
+                  <li></li>
+                  <li>TEL : </li>
+                  <li>OPEN : </li>"
+                  </ul>
+                  <footer class="footer-stockist">
+                  <a href="" target="_blank" rel="noopener noreferrer">Google Maps</a>
+                  <a href="" width="29" height="29" target="_blank" rel="noopener noreferrer"><img src="src/assets/logo-instagram.svg" alt="Instagram" /></a>"
+                  </footer>
+                </div>
+
+                <div class=flex-item">
+                  <img src="src/assets/stockist/img-stockist-5.webp" alt="Store" class="image-fluid" loading="lazy" decoding="async" />
+                  <h2 class="font-EBGaramond"></h2>
+                  <ul>
+                  <li></li>
+                  <li>TEL : </li>
+                  <li>OPEN : </li>"
+                  </ul>
+                  <footer class="footer-stockist">
+                  <a href="" target="_blank" rel="noopener noreferrer">Google Maps</a>
+                  <a href="" width="29" height="29" target="_blank" rel="noopener noreferrer"><img src="src/assets/logo-instagram.svg" alt="Instagram" /></a>"
+                  </footer>
+                </div>
+              </div>
+
+                <div class=flex-item">
+                  <img src="src/assets/stockist/img-stockist-6.webp" alt="Store" class="image-fluid" loading="lazy" decoding="async" />
+                  <h2 class="font-EBGaramond"></h2>
+                  <ul>
+                  <li></li>
+                  <li>TEL : </li>
+                  <li>OPEN : </li>"
+                  </ul>
+                  <footer class="footer-stockist">
+                  <a href="" target="_blank" rel="noopener noreferrer">Google Maps</a>
+                  <a href="" width="29" height="29" target="_blank" rel="noopener noreferrer"><img src="src/assets/logo-instagram.svg" alt="Instagram" /></a>"
+                  </footer>
+                </div>
+
+                <div class=flex-item">
+                  <img src="src/assets/stockist/img-stockist-7.webp" alt="Store" class="image-fluid" loading="lazy" decoding="async" />
+                  <h2 class="font-EBGaramond"></h2>
+                  <ul>
+                  <li></li>
+                  <li>TEL : </li>
+                  <li>OPEN : </li>"
+                  </ul>
+                  <footer class="footer-stockist">
+                  <a href="" target="_blank" rel="noopener noreferrer">Google Maps</a>
+                  <a href="" width="29" height="29" target="_blank" rel="noopener noreferrer"><img src="src/assets/logo-instagram.svg" alt="Instagram" /></a>"
+                  </footer>
+                </div>
+                
+                <div class=flex-item">
+                  <img src="src/assets/stockist/img-stockist-8.webp" alt="Store" class="image-fluid" loading="lazy" decoding="async" />
+                  <h2 class="font-EBGaramond"></h2>
+                  <ul>
+                  <li></li>
+                  <li>TEL : </li>
+                  <li>OPEN : </li>"
+                  </ul>
+                  <footer class="footer-stockist">
+                  <a href="" target="_blank" rel="noopener noreferrer">Google Maps</a>
+                  <a href="" width="29" height="29" target="_blank" rel="noopener noreferrer"><img src="src/assets/logo-instagram.svg" alt="Instagram" /></a>"
+                  </footer>
+                </div>
+              </div>
+              
+            </div>
+
+            <section class="container-xl container-stockist">
+              <div class="row">
+                <div class="col">
+                  <h1 class="font-EBGaramond">FLAGSHIP STORE</h1>
+                
+                  <column>
+                    <div class=column-item">
+                      <h2 class="font-EBGaramond">JAPAN</h2>
+                      <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                      </ul>
+                    </div>
+
+                    <div class=column-item">
+                      <h2 class="font-EBGaramond"></h2>
+                      <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                      </ul>
+                    </div>
+
+                    <div class=column-item">
+                      <h2 class="font-EBGaramond"></h2>
+                      <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                      </ul>
+                    </div>
+
+                    <div class=column-item">
+                      <h2 class="font-EBGaramond"></h2>
+                      <ul>
+                        <li></li>
+                      </ul>
+                    </div>
+
+                    <div class=column-item">
+                      <h2 class="font-EBGaramond"></h2>
+                      <ul>
+                        <li></li>
+                        <li></li>
+                      </ul>
+                    </div>
+
+                    <div class=column-item">
+                      <h2 class="font-EBGaramond"></h2>
+                      <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                      </ul>
+                    </div>
+
+                    <div class=column-item">
+                      <h2 class="font-EBGaramond"></h2>
+                      <ul>
+                        <li></li>
+                        <li></li>
+                      </ul>
+                    </div>
+
+                    <div class=column-item">
+                      <h2 class="font-EBGaramond"></h2>
+                      <ul>
+                        <li></li>
+                        <li></li>
+                      </ul>
+                    </div>
+                  </column>
+                </div>
+              </div>
+            </section>
+              
+          </div>
         </section>
       </main>
     `;
